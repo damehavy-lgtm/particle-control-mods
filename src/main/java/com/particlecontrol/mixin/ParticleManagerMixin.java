b7f4473992b732030a8198d842c0c119265e7e89
damehavy-lgtm <damehavy@gmail.com>
2026-03-28 16:46:19 +0300

    Update ParticleManagerMixin.java

--- src/main/java/com/particlecontrol/mixin/ParticleManagerMixin.java
+++ src/main/java/com/particlecontrol/mixin/ParticleManagerMixin.java
@@ -4,7 +4,6 @@ import com.particlecontrol.client.ParticleContext;
 import com.particlecontrol.config.ParticleControlConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.particle.ParticleEffect;
@@ -13,13 +12,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Random;
-
 @Environment(EnvType.CLIENT)
 @Mixin(ParticleManager.class)
 public class ParticleManagerMixin {
-
-    private static final Random RANDOM = new Random();
 
     @Inject(
         method = "addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)Lnet/minecraft/client/particle/Particle;",
@@ -32,44 +27,9 @@ public class ParticleManagerMixin {
                                CallbackInfoReturnable<Particle> cir) {
 
         ParticleControlConfig cfg = ParticleControlConfig.getInstance();
-        MinecraftClient client = MinecraftClient.getInstance();
-        ParticleContext.ParticleSource source = ParticleContext.classify(client);
 
-        switch (source) {
-            case SELF -> {
-                if (cfg.hideOwnParticles) {
-                    cir.setReturnValue(null);
-                    return;
-                }
-            }
-            case ENEMY -> {
-                if (!cfg.showEnemyParticles) {
-                    cir.setReturnValue(null);
-                    return;
-                }
-                if (!shouldSpawn(cfg.enemyParticleMultiplier)) {
-                    cir.setReturnValue(null);
-                    return;
-                }
-            }
-            case NEUTRAL -> {
-                if (!shouldSpawn(cfg.neutralParticleMultiplier)) {
-                    cir.setReturnValue(null);
-                    return;
-                }
-            }
-            case ENVIRONMENT -> {
-                if (cfg.hideEnvironmentParticles) {
-                    cir.setReturnValue(null);
-                    return;
-                }
-            }
+        if (cfg.hideOwnParticles && ParticleContext.isLocalPlayer()) {
+            cir.setReturnValue(null);
         }
-    }
-
-    private static boolean shouldSpawn(float multiplier) {
-        if (multiplier <= 0f) return false;
-        if (multiplier >= 1f) return true;
-        return RANDOM.nextFloat() < multiplier;
     }
 }
